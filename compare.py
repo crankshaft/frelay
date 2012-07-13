ca3aeee7bd98ad7d3432d39259383838dd2c7173
clumsy <crankshaft@sina.cn>
2012-07-14 01:10:25 +0800

    new feature: compare between url and md5sum

--- compare.py
+++ compare.py
@@ -29,6 +29,23 @@ def compare_url_path(url, path) :
 
 	return False
 
+def compare_url_md5(url, md5) :
+	m = hashlib.md5()
+	try :
+		http = httplib.HTTPConnection("localhost", timeout=60)
+		http.request(method="GET", url=url)
+		ret = http.getresponse()
+		if (int(ret.status) == 200) :
+			body = ret.read()
+			m.update(body)
+
+			if (m.hexdigest() == md5) :
+				return True
+	except :
+	   return False
+
+	return False
+
 def main() :
 
 	while True:
@@ -43,13 +60,16 @@ def main() :
 		except :
 			return
 
-		(url, path) = msg.split(' ');
+		(url, path, md5) = msg.split(' ');
 
+		'''
 		ret = compare_url_path(url, path)
+		'''
+		ret = compare_url_md5(url, md5)
 		if ret :
-			print "succ:", url, path
+			print "succ:", url, path, md5
 		else :
-			print "failed:", url, path
+			print "failed:", url, path, md5
 
 
 if __name__ == "__main__" :
